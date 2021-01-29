d33af16c4b917da68ba08cb5c0deff6970fdbff6
Michael Burton <micbur1@gmail.com>
2021-01-29 10:10:00 -0800

    add ready function for submitted form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business logic
 // logic for pizzas
  function pizza(pizzaNum, toppings, size, price){
    this.pizzaNum = pizzaNum;
@@ -5,3 +6,16 @@
    this.size = size;
    this.price = price;
  }
+
+
+
+ // User logic
+ $(document).ready(function() {
+    $("form#toppings-survey").submit(function(event) {
+      event.preventDefault();
+      $("input:checkbox[name=pizza_toppings]:checked").each(function() {
+        const allPizzaToppings= $(this).val();
+      });
+      console.log(allPizzaToppings);
+    });
+  });
