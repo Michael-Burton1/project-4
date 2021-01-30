b6afaec2227e84aecdf30c876f972eaffda4c0b4
Michael Burton <micbur1@gmail.com>
2021-01-29 16:39:24 -0800

    cdelete commented out unused code. try to add a total cost for 'additionalCost'.

--- js/scripts.js
+++ js/scripts.js
@@ -11,36 +11,41 @@ Orders.prototype.addOrder= function(pizza){
   this.pizzas[pizza.id] = pizza;
   console.log(ugh);
 }
+
+
 // logic for pizzas
- function pizza(pizzaNum, toppings, size, price){
-   this.pizzaNum = pizzaNum;
-   this.toppings = toppings;
-   this.size = size;
-   this.price = price;
- }
+function Pizza(pizzaNum, toppings, size, price){
+  this.pizzaNum = pizzaNum;
+  this.toppings = toppings;
+  this.size = size;
+  this.price = price;
+}
 
-
-  // pizza.prototype.assignId = function() {
-  //   this.currentId += 1;
-  //   return this.currentId;
-  // }
-
-      
-  
-  // User logic
-  toppingsArray = []
-  $(document).ready(function() {
-    $("form#toppings_survey").submit(function(event) {
-      event.preventDefault();
-      $("input:checkbox[name=pizza_toppings]:checked").each(function() {
-        const allPizzaToppings= $(this).val();
-        toppingsArray.push(allPizzaToppings)
-        let toppingsCost = toppingsArray.map((i) => Number(i));
-        console.log("toppingscost", toppingsCost);
-
-      //  console.log(allPizzaToppings);
-      });
-      // toppingsArray.forEach(function(element) {
-      //   sum += element;
-    });
+Pizza.prototype.additonalCost= function(){
+  let sum= 0;
+  toppingCost.forEach(function(element) {
+    sum +=element;
+    console.log(sum)
+    this.toppings.push(sum);
   });
+};  
+toppingsArray = []
+
+
+    
+
+  // User logic
+$(document).ready(function() {
+  $("form#toppings_survey").submit(function(event) {
+    event.preventDefault();
+    $("input:checkbox[name=pizza_toppings]:checked").each(function() {
+      const allPizzaToppings= $(this).val();
+      toppingsArray.push(allPizzaToppings)
+      let toppingsCost = toppingsArray.map((i) => Number(i));
+      this.additionalCost();
+      console.log(AdditionalCost);
+
+    });
+    
+  });
+});
