e22db4b6924deedc296b97dbc04dc861b539f5ba
Michael Burton <micbur1@gmail.com>
2021-02-07 15:41:14 -0800

    add toppings prototype to Pizza

--- js/scripts2.js
+++ js/scripts2.js
@@ -7,9 +7,16 @@ function Pizza(sizeCost, toppingsCost){
 
 let Pizza1 = new Pizza(12, 4);
 console.log(Pizza1);
+let P2 = new Pizza(12,[1,1]);
+console.log(P2);
 
-Pizza.prototype.totalCost = function(){
+Pizza.prototype.total = function(){
   let totalCost = this.sizeCost + this.toppingsCost
   console.log(totalCost)
   return totalCost
+};
+
+Pizza.prototype.toppings= function(){
+  let toppingsTotal = this. toppingsCost.reduce((a, b) => a + b, 0);
+  console.log(toppingsTotal);
 }
